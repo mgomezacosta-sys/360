37536c071840c2dd1f7df4246128e9ecef6b1558
mgomezacosta-sys <mgomezacosta@ugr.edu.ar>
2026-01-21 14:53:11 -0300

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-5969540_1280",
+      "name": "panorama-5969540_1280",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 1.0947287999533781,
+        "pitch": 0.4616517080295104,
+        "fov": 1.273632630139061
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.867896651335883,
+          "pitch": 0.45237928220134194,
+          "rotation": 0,
+          "target": "1-hong-kong-7361979_1280"
+        },
+        {
+          "yaw": 0.6788777944305942,
+          "pitch": 0.0161363204372158,
+          "rotation": 0,
+          "target": "2-winter-2383930_1280"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hong-kong-7361979_1280",
+      "name": "hong-kong-7361979_1280",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-winter-2383930_1280",
+      "name": "winter-2383930_1280",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "prueba21/1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
